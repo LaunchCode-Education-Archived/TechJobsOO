86aba45c6d245ac60ae46fb327047820a2576414
Chris Bay <chris@chrisbay.net>
2017-03-21 17:24:06 -0500

    WIP - Add job functionality

---
+++ src/TechJobs/Controllers/JobController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+using TechJobs.ViewModels;
+
+namespace TechJobs.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("New");
+        }
+
+        public IActionResult New()
+        {
+            NewJobViewModel newJobViewModel = new NewJobViewModel();
+            return View(newJobViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult New(NewJobViewModel newJobViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // create new job object
+                return Redirect("New");
+            }
+
+            return View(newJobViewModel);
+        }
+    }
+}
---
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class NewJobViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Employer")]
+        public int EmployerID { get; set; }
+
+        public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
+
+        public NewJobViewModel()
+        {
+            foreach (JobField field in JobData.FindAll(JobFieldType.Employer))
+            {
+                Employers.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+        }
+    }
+}
